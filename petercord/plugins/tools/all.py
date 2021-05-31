014a85aba0d867fe37e1e60c4ab343b2156eeef8
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-31 22:17:25 +0700

    Create all.py

---
+++ petercord/plugins/tools/all.py
@@ -0,0 +1,12 @@
+from petercord import Message, petercord
+
+from .. import get_all_plugins
+
+
+@petercord.on_cmd("all", about={"header": "list all plugins in plugins/ path"})
+async def getplugins(message: Message):
+    raw_ = get_all_plugins()
+    out_str = f"**--({len(raw_)}) Plugins Available!--**\n\n"
+    for plugin in ("/".join(i.split(".")) for i in raw_):
+        out_str += f"    `{plugin}.py`\n"
+    await message.edit(text=out_str, del_in=0)
