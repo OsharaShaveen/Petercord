b385bc60b346aae08590143a767a258044da7fbe
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-05-31 13:43:55 +0700

    Create __init__.py

---
+++ petercord/core/__init__.py
@@ -0,0 +1,8 @@
+# petercord
+
+from pyrogram import filters  # noqa
+
+from .database import get_collection  # noqa
+from .ext import pool  # noqa
+from .types.bound import Message  # noqa
+from .client import Petercord  # noqa
