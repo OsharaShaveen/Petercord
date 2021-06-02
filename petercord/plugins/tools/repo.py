923f6a7691f91fd6f912a93c8b92787533cc4386
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2021-06-02 09:42:58 +0700

    Update repo.py

--- petercord/plugins/tools/repo.py
+++ petercord/plugins/tools/repo.py
@@ -1,10 +1,17 @@
 # petercord
 
-from petercord import Config, Message, petercord
+from petercord import petercord, Message, Config, versions, get_version
 
 
-@petercord.on_cmd("repo", about={"header": "get repo link and details"})
+@petercord.on_cmd("repo", about={'header': "get repo link and details"})
 async def see_repo(message: Message):
     """see repo"""
-    output = f"• **repo** : [PETERCORD]({Config.UPSTREAM_REPO})"
+    output = f"""
+**HI**, __WELCOME PETERCORD USERBOT__ 🎖 **PETERCORD** 🎖
+    __DATA PETERCORD__
+• **PETERCORD VERSION** : `{get_version()}`
+• **LICENSE** : {versions.__license__}
+• **COPYRIGHT** : {versions.__copyright__}
+• **REPO** : [PETERCORD]({Config.UPSTREAM_REPO})
+"""
     await message.edit(output)
