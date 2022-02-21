0536d04327b9d80496696ee84783a52854cb5409
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2022-02-21 12:08:23 +0700

    Update checks.sh

--- init/checks.sh
+++ init/checks.sh
@@ -1,9 +1,5 @@
 #PETERCORD
 
-import os
-os.system("pip3 install --no-cache-dir -U -q -r telethon.txt")
-    
-
 _checkBashReq() {
     log "Checking Bash Commands ..."
     command -v jq &> /dev/null || quit "Required command : jq : could not be found !"
