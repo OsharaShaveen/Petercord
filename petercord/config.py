53469907ec40ca108fcb39ac941c38cc054f663e
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2022-02-27 21:54:53 +0700

    Update config.py

--- petercord/config.py
+++ petercord/config.py
@@ -45,6 +45,7 @@ class Config:
     CACHE_PATH = "petercord/xcache"
     CMD_TRIGGER = os.environ.get("CMD_TRIGGER")
     SUDO_TRIGGER = os.environ.get("SUDO_TRIGGER")
+    PUBLIC_TRIGGER = '/'
     FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR")
     UNFINISHED_PROGRESS_STR = os.environ.get("UNFINISHED_PROGRESS_STR")
     ALIVE_MEDIA = os.environ.get("ALIVE_MEDIA")
