337b5e9741eb2fcd0d95154027974b17e46b4aa1
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2022-02-27 22:16:25 +0700

    Update client.py

--- petercord/core/client.py
+++ petercord/core/client.py
@@ -2,23 +2,27 @@
 
 __all__ = ['Petercord']
 
-import os
-import time
-import signal
 import asyncio
+import functools
 import importlib
+import inspect
+import os
+import signal
+import threading
+import time
 from types import ModuleType
 from typing import List, Awaitable, Any, Optional, Union
 
-from pyrogram import idle
+from pyrogram import idle, types
+from pyrogram.methods import Methods as RawMethods
 
 from petercord import logging, Config, logbot
+from petercord.plugins import get_all_plugins
 from petercord.utils import time_formatter
 from petercord.utils.exceptions import PetercordBotNotFound
-from petercord.plugins import get_all_plugins
+from .database import get_collection
+from .ext import RawClient
 from .methods import Methods
-from .ext import RawClient, pool
-from .database import _close_db
 
 _LOG = logging.getLogger(__name__)
 _LOG_STR = "<<<!  #####  %s  #####  !>>>"
@@ -27,6 +31,24 @@ _IMPORTED: List[ModuleType] = []
 _INIT_TASKS: List[asyncio.Task] = []
 _START_TIME = time.time()
 _SEND_SIGNAL = False
+
+_PETERCORD_STATUS = get_collection("PETERCORD_STATUS")
+
+
+async def _set_running(is_running: bool) -> None:
+    await _PETERCORD_STATUS.update_one(
+        {'_id': 'PETERCORD_STATUS'},
+        {"$set": {'is_running': is_running}},
+        upsert=True
+    )
+
+
+async def _is_running() -> bool:
+    if Config.ASSERT_SINGLE_INSTANCE:
+        data = await _PETERCORD_STATUS.find_one({'_id': 'PETERCORD_STATUS'})
+        if data:
+            return bool(data['is_running'])
+    return False
 
 
 async def _complete_init_tasks() -> None:
@@ -37,6 +59,17 @@ async def _complete_init_tasks() -> None:
 
 
 class _AbstractPetercord(Methods, RawClient):
+    def __init__(self, **kwargs) -> None:
+        self._me: Optional[types.User] = None
+        super().__init__(**kwargs)
+
+    @property
+    def id(self) -> int:
+        """ returns client id """
+        if self.is_bot:
+            return RawClient.BOT_ID
+        return RawClient.USER_ID
+
     @property
     def is_bot(self) -> bool:
         """ returns client is bot or not """
@@ -46,7 +79,7 @@ class _AbstractPetercord(Methods, RawClient):
 
     @property
     def uptime(self) -> str:
-        """ returns petercord uptime """
+        """ returns userge uptime """
         return time_formatter(time.time() - _START_TIME)
 
     async def finalize_load(self) -> None:
@@ -54,7 +87,7 @@ class _AbstractPetercord(Methods, RawClient):
         await asyncio.gather(_complete_init_tasks(), self.manager.init())
 
     async def load_plugin(self, name: str, reload_plugin: bool = False) -> None:
-        """ Load plugin to Petercord """
+        """ Load plugin to Petercord-X """
         _LOG.debug(_LOG_STR, f"Importing {name}")
         _IMPORTED.append(
             importlib.import_module(f"petercord.plugins.{name}"))
@@ -75,7 +108,7 @@ class _AbstractPetercord(Methods, RawClient):
         for name in get_all_plugins():
             try:
                 await self.load_plugin(name)
-            except ImportError as i_e:
+            except Exception as i_e:
                 _LOG.error(_LOG_STR, f"[{name}] - {i_e}")
         await self.finalize_load()
         _LOG.info(_LOG_STR, f"Imported ({len(_IMPORTED)}) Plugins => "
@@ -89,7 +122,7 @@ class _AbstractPetercord(Methods, RawClient):
         for imported in _IMPORTED:
             try:
                 reloaded_ = importlib.reload(imported)
-            except ImportError as i_e:
+            except Exception as i_e:
                 _LOG.error(_LOG_STR, i_e)
             else:
                 reloaded.append(reloaded_.__name__)
@@ -97,11 +130,30 @@ class _AbstractPetercord(Methods, RawClient):
         await self.finalize_load()
         return len(reloaded)
 
+    async def get_me(self, cached: bool = True) -> types.User:
+        if not cached or self._me is None:
+            self._me = await super().get_me()
+        return self._me
+
+    async def start(self):
+        await super().start()
+        self._me = await self.get_me()
+        if self.is_bot:
+            RawClient.BOT_ID = self._me.id
+        else:
+            RawClient.USER_ID = self._me.id
+
+    def __eq__(self, o: object) -> bool:
+        return isinstance(o, _AbstractPetercord) and self.id == o.id
+
+    def __hash__(self) -> int:  # pylint: disable=W0235
+        return super().__hash__()
+
 
 class PetercordBot(_AbstractPetercord):
-    """ Petercord-X Bot """
+    """ UsergeBot, the bot """
     def __init__(self, **kwargs) -> None:
-        _LOG.info(_LOG_STR, "Setting X-BOT Configs")
+        _LOG.info(_LOG_STR, "Setting Petercord-X Configs")
         super().__init__(session_name=":memory:", **kwargs)
 
     @property
@@ -111,12 +163,12 @@ class PetercordBot(_AbstractPetercord):
 
 
 class Petercord(_AbstractPetercord):
-    """ Petercord, the userbot """
+    """ Userge, the userbot """
 
     has_bot = bool(Config.BOT_TOKEN)
 
     def __init__(self, **kwargs) -> None:
-        _LOG.info(_LOG_STR, "Setting Petercord-X Configs")
+        _LOG.info(_LOG_STR, "Setting Petercord Configs")
         kwargs = {
             'api_id': Config.API_ID,
             'api_hash': Config.API_HASH,
@@ -129,16 +181,14 @@ class Petercord(_AbstractPetercord):
             kwargs['bot'] = PetercordBot(bot=self, **kwargs)
         kwargs['session_name'] = Config.HU_STRING_SESSION or ":memory:"
         super().__init__(**kwargs)
-        self.executor.shutdown()
-        self.executor = pool._get()  # pylint: disable=protected-access
-    
+
     @property
     def dual_mode(self) -> bool:
         return RawClient.DUAL_MODE
 
     @property
     def bot(self) -> Union['PetercordBot', 'Petercord']:
-        """ returns petercordbot """
+        """ returns usergebot """
         if self._bot is None:
             if Config.BOT_TOKEN:
                 return self
@@ -147,27 +197,48 @@ class Petercord(_AbstractPetercord):
 
     async def start(self) -> None:
         """ start client and bot """
+        counter = 0
+        timeout = 30  # 30 sec
+        max_ = 1800  # 30 min
+
+        while await _is_running():
+            _LOG.info(_LOG_STR, "Waiting for the Termination of "
+                                f"previous Userge instance ... [{timeout} sec]")
+            time.sleep(timeout)
+
+            counter += timeout
+            if counter >= max_:
+                _LOG.info(_LOG_STR, f"Max timeout reached ! [{max_} sec]")
+                break
+
         _LOG.info(_LOG_STR, "Starting Petercord-X")
+        await _set_running(True)
         await super().start()
         if self._bot is not None:
-            _LOG.info(_LOG_STR, "Starting Asisten-Bot")
+            _LOG.info(_LOG_STR, "Starting Asisten")
             await self._bot.start()
         await self._load_plugins()
 
     async def stop(self) -> None:  # pylint: disable=arguments-differ
         """ stop client and bot """
         if self._bot is not None:
-            _LOG.info(_LOG_STR, "Stopping Asisten-Bot")
+            _LOG.info(_LOG_STR, "Stopping Asisten")
             await self._bot.stop()
         _LOG.info(_LOG_STR, "Stopping Petercord-X")
         await super().stop()
-        _close_db()
-        pool._stop()  # pylint: disable=protected-access
+        await _set_running(False)
 
     def begin(self, coro: Optional[Awaitable[Any]] = None) -> None:
-        """ start petercord """
+        """ start Petercord-X """
         lock = asyncio.Lock()
+        loop_is_stopped = asyncio.Event()
         running_tasks: List[asyncio.Task] = []
+
+        async def _waiter() -> None:
+            try:
+                await asyncio.wait_for(loop_is_stopped.wait(), 30)
+            except asyncio.exceptions.TimeoutError:
+                pass
 
         async def _finalize() -> None:
             async with lock:
@@ -175,18 +246,16 @@ class Petercord(_AbstractPetercord):
                     t.cancel()
                 if self.is_initialized:
                     await self.stop()
-                else:
-                    _close_db()
-                    pool._stop()  # pylint: disable=protected-access
             # pylint: disable=expression-not-assigned
             [t.cancel() for t in asyncio.all_tasks() if t is not asyncio.current_task()]
             await self.loop.shutdown_asyncgens()
             self.loop.stop()
             _LOG.info(_LOG_STR, "Loop Stopped !")
+            loop_is_stopped.set()
 
         async def _shutdown(_sig: signal.Signals) -> None:
             global _SEND_SIGNAL  # pylint: disable=global-statement
-            _LOG.info(_LOG_STR, f"Received Stop Signal [{_sig.name}], Exiting Petercord-X Gagal Membuka ...")
+            _LOG.info(_LOG_STR, f"Received Stop Signal [{_sig.name}], Exiting Petercord-X ...")
             await _finalize()
             if _sig == _sig.SIGUSR1:
                 _SEND_SIGNAL = True
@@ -194,24 +263,66 @@ class Petercord(_AbstractPetercord):
         for sig in (signal.SIGHUP, signal.SIGTERM, signal.SIGINT, signal.SIGUSR1):
             self.loop.add_signal_handler(
                 sig, lambda _sig=sig: self.loop.create_task(_shutdown(_sig)))
-        self.loop.run_until_complete(self.start())
+
+        try:
+            self.loop.run_until_complete(self.start())
+        except RuntimeError:
+            return
+
         for task in self._tasks:
             running_tasks.append(self.loop.create_task(task()))
-        logbot.edit_last_msg("Petercord-X has Started Successfully !")
+
+        logbot.edit_last_msg("Userge has Started Successfully !")
         logbot.end()
         mode = "[DUAL]" if RawClient.DUAL_MODE else "[BOT]" if Config.BOT_TOKEN else "[USER]"
+
         try:
             if coro:
                 _LOG.info(_LOG_STR, f"Running Coroutine - {mode}")
                 self.loop.run_until_complete(coro)
             else:
-                _LOG.info(_LOG_STR, f"Idling Petercord-X - {mode}")
+                _LOG.info(_LOG_STR, f"Idling Userge - {mode}")
                 idle()
             self.loop.run_until_complete(_finalize())
         except (asyncio.exceptions.CancelledError, RuntimeError):
             pass
         finally:
-            self.loop.close()
-            _LOG.info(_LOG_STR, "Loop Closed !")
+            try:
+                self.loop.run_until_complete(_waiter())
+            except RuntimeError:
+                pass
             if _SEND_SIGNAL:
                 os.kill(os.getpid(), signal.SIGUSR1)
+
+
+def _un_wrapper(obj, name, function):
+    loop = asyncio.get_event_loop()
+
+    @functools.wraps(function)
+    def _wrapper(*args, **kwargs):
+        coroutine = function(*args, **kwargs)
+        if (threading.current_thread() is not threading.main_thread()
+                and inspect.iscoroutine(coroutine)):
+            async def _():
+                return await asyncio.wrap_future(asyncio.run_coroutine_threadsafe(coroutine, loop))
+            return _()
+        return coroutine
+
+    setattr(obj, name, _wrapper)
+
+
+def _un_wrap(source):
+    for name in dir(source):
+        if name.startswith("_"):
+            continue
+        wrapped = getattr(getattr(source, name), '__wrapped__', None)
+        if wrapped and (inspect.iscoroutinefunction(wrapped)
+                        or inspect.isasyncgenfunction(wrapped)):
+            _un_wrapper(source, name, wrapped)
+
+
+_un_wrap(RawMethods)
+for class_name in dir(types):
+    cls = getattr(types, class_name)
+    if inspect.isclass(cls):
+        _un_wrap(cls)
