4f1b040a4cae6ace3ad246af8d71d21e97b61cb0
IlhamMansiez <84231496+IlhamMansiez@users.noreply.github.com>
2022-03-07 10:25:11 +0700

    Update __main__.py

--- petercord/__main__.py
+++ petercord/__main__.py
@@ -1,21 +1,7 @@
 # petercord
 
 from petercord import petercord
-CHANNEL = petercord.getCLogger(__name__)
-from .config import get_version
 
-ON = f"""
-👥 **Petercord-X Aktif**
-•••••••••••
-💻 **Version -** `{get_version()}`
-
-❗Sebaiknya Anda jangan keluar grup ini agar bot tidak mati
- ....Terimakasih....🇮🇩
-❗You should not leave this group so that the bot does not die
- ....Thank You....🇺🇸
-•••••••••••
-"""
 
 if __name__ == "__main__":
-    await CHANNEL.log(ON)
     petercord.begin()
